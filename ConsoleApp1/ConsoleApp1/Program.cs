4a8f244d770df39453c811dd7e95ac0e5128b3b4
sviridov <sviridov97@gmail.com>
2025-12-14 20:01:14 +0500

    practic

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -5,8 +5,7 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
-            ConsoleApp1.WriteLine("Привет, Мир!");
-            Console.WriteLine("Привет, Артем!");
+            
         }
     }
 }
